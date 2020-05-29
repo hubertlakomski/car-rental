e88da11e2eed1903f7b6f7e3f50ca0f98836f841
Hubert Łakomski <hubertlakomski@gmail.com>
2020-05-29 19:22:09 +0200

    implement serializable for parent entity

--- src/main/java/pl/hubertlakomski/carrental/domain/model/ParentEntity.java
+++ src/main/java/pl/hubertlakomski/carrental/domain/model/ParentEntity.java
@@ -6,10 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
 @Getter @Setter @EqualsAndHashCode(of = "id") @ToString(of = "id")
 @MappedSuperclass
-public abstract class ParentEntity {
+public abstract class ParentEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
