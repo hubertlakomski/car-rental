225a3cbc609e920634e15419aea9464824bc8a94
Hubert Łakomski <hubertlakomski@gmail.com>
2020-06-21 17:40:33 +0200

    deleting unnecessary imports

--- src/main/java/pl/hubertlakomski/carrental/CarRentalApplication.java
+++ src/main/java/pl/hubertlakomski/carrental/CarRentalApplication.java
@@ -3,9 +3,6 @@ package pl.hubertlakomski.carrental;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import pl.hubertlakomski.carrental.domain.model.Rental;
-import pl.hubertlakomski.carrental.domain.repository.RentalRepository;
 
 @SpringBootApplication @Slf4j
 public class CarRentalApplication {
