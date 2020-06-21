6a2c3fabb93da6a43bb441aa53041fc41583c639
Hubert Łakomski <hubertlakomski@gmail.com>
2020-06-21 17:24:07 +0200

    removal unnecessary imports

--- src/main/java/pl/hubertlakomski/carrental/domain/repository/RoleRepository.java
+++ src/main/java/pl/hubertlakomski/carrental/domain/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package pl.hubertlakomski.carrental.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.hubertlakomski.carrental.domain.model.users.Role;
 
+import java.util.List;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role getByName(String role_user);
 }
