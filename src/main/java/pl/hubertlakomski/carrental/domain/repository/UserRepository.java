038d5beb8f2a80e2d17581e785e16eb49c238402
Hubert Łakomski <hubertlakomski@gmail.com>
2020-06-21 17:25:15 +0200

    add method to find user by username and find user by id with they roles and find user by id

--- src/main/java/pl/hubertlakomski/carrental/domain/repository/UserRepository.java
+++ src/main/java/pl/hubertlakomski/carrental/domain/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package pl.hubertlakomski.carrental.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.hubertlakomski.carrental.domain.model.users.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.id = ?1")
+    User findUserByIdJoinRoles(Long userId);
+
+    Optional<User> findById(Long id);
 }
